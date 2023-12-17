d5ed930e39b59136d86b465bc0848ee4dc162cf3
osatoh <sugar961216@gmail.com>
2023-12-17 22:50:52 +0900

    done hoge

--- 02_object_model/01_hoge.rb
+++ 02_object_model/01_hoge.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Q1.
 # Hogeクラスは次の仕様を持つ
 # "hoge" という文字列の定数Hogeを持つ
@@ -5,7 +7,17 @@
 # HogeクラスのスーパークラスはStringである
 # 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義されている
 
-class Hoge
+# このHogeクラスを定義する
+class Hoge < String
+  Hoge = 'hoge'
+
+  def hogehoge
+    Hoge
+  end
+
+  def hoge?
+    self == Hoge
+  end
 end
 
 # Q2.
@@ -17,3 +29,11 @@ end
 # - Class
 # - Hash
 # - TrueClass
+
+# class String < Hoge; end
+
+class Object
+  def hoge
+    'hoge'
+  end
+end
